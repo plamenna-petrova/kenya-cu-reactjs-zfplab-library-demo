9b092c9f2b3912c5f426b1a7710937522289d4d9
plamenna.petrova <plamennavp@abv.bg>
2024-10-24 11:09:59 +0300

    Returned the disabling of side menu items

--- src/components/navigation-drawer/NavigationDrawer.jsx
+++ src/components/navigation-drawer/NavigationDrawer.jsx
@@ -447,7 +447,7 @@ export const NavigationDrawer = () => {
                     },
                 ]}
                 onClick={() => showSection(sidebarMenuItem.title)}
-                // disabled={!zfpLabServerConnectionState.isConnected || !fiscalDeviceConnectionState.isConnected}
+                disabled={!zfpLabServerConnectionState.isConnected || !fiscalDeviceConnectionState.isConnected}
               >
                 <ListItemIcon
                   sx={[
