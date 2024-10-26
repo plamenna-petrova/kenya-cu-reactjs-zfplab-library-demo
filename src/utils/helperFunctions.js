6f00ea6b0ce74f3a00b1e6afe88c2d32640c0684
plamenna.petrova <plamennavp@abv.bg>
2024-10-26 16:01:15 +0300

    Removed empty row

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,4 +1,3 @@
-
 export const isNullOrWhitespace = (input) => !input || input.trim().length === 0;
 
 export const sleepAsync = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
